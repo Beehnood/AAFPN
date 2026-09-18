27911c98060fac90df053decca06bb4ff7d62b22
Beehnood <behnoodpazhang67@gmail.com>
2026-09-18 17:05:09 +0200

    develop: cote front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main className="container">
+      <div className="flex min-h-screen items-center justify-center bg-slate-100">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-blue-600">
+            Association d'Amitié Franco-Persane de Nice
+          </h1>
+
+          <p className="mt-4 text-lg text-slate-600">
+            Bienvenue sur notre site
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
